62aa35d4bc7a75f679d36bea19c32b06670263d8
Nikita Pathak <nikitapathak100@gmail.com>
2019-10-20 15:13:16 +0530

    my project setup completed

---
+++ shopping-backend/src/main/java/pathak/nikita/shopping_backend/App.java
@@ -0,0 +1,13 @@
+package pathak.nikita.shopping_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
